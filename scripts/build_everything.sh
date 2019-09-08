0ba3415af4a5873087acff44ba745dd0188a04df
nobody <nobody@nowhere.com>
2019-09-08 20:43:19 +0800

    ..

--- scripts/build_everything.sh
+++ scripts/build_everything.sh
@@ -2,7 +2,7 @@ QTAV=$HOME/android/QtAV
 is_sudoer=`sudo -nv 2>&1`
 is_sudoer=${is_sudoer%%,*}
 if [[ `sudo -nv 2>&1` =~ "password" ]]; then
-    pkgs='build-essential default-jre openjdk-8-jdk-headless android-sdk android-sdk-platform-23 libc6-i386 openjdk-8-jdk libc6-i386'
+    pkgs='git build-essential default-jre openjdk-8-jdk-headless android-sdk android-sdk-platform-23 libc6-i386 openjdk-8-jdk libc6-i386'
     if ! dpkg -s $pkgs >/dev/null 2>&1; then
         sudo apt-get install $pkgs
     fi
