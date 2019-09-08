d09f243308327e1b726d8d07476fff76ed00b40d
nobody <nobody@nowhere.com>
2019-09-08 22:45:02 +0800

    ...

--- scripts/build_everything.sh
+++ scripts/build_everything.sh
@@ -1,11 +1,17 @@
 QTAV=$HOME/android/QtAV
 is_sudoer=`sudo -nv 2>&1`
 is_sudoer=${is_sudoer%%,*}
-if [[ `sudo -nv 2>&1` =~ "password" ]]; then
+echo "xxxx"
+if [[ ! `sudo -nv 2>&1` =~ "Sorry" ]]; then
+    echo "I am sudoer"
     pkgs='git build-essential default-jre openjdk-8-jdk-headless android-sdk android-sdk-platform-23 libc6-i386 openjdk-8-jdk libc6-i386'
-    if ! dpkg -s $pkgs >/dev/null 2>&1; then
-        sudo apt-get install $pkgs
-    fi
+    for mpkg in $pkgs; do
+        echo $mpkg
+        if [ $(dpkg-query -W -f='${Status}' $mpkg 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
+            echo "installing $mpkg"
+            sudo apt-get install $mpkg
+        fi
+    done
     pkgs='openjdk-12-jdk openjdk-12-jre'
     for mpkg in $pkgs; do
         if dpkg -s $mpkg >/dev/null 2>&1; then
@@ -20,10 +26,11 @@ if [[ `sudo -nv 2>&1` =~ "password" ]]; then
 fi
 echo "update QtAV"
 cd $QTAV
-git config user.email nobody@nowhere.com
-git config user.name nobody
-git stash
-git clean -f
-git pull
-
+if [ -f /usr/bin/git ]; then
+    git config user.email nobody@nowhere.com
+    git config user.name nobody
+    git stash
+    git clean -f
+    git pull
+fi
 $QTAV/scripts/build_everything_actual.sh
